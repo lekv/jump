d70e4972b04d0329391be062b685b110f1587fae
Philip Zeyliger <philip@cloudera.com>
2013-10-13 15:04:28 -0700

    cloudera

--- data.js
+++ data.js
@@ -1,16 +1,19 @@
 var data = [
   { 
+    'name': 'Internal JIRA',
     're': '(opsaps|cdh|kitchen|helpdesk)[- ](\\d+)', 
     'captures': [ "project", "id" ], 
     'link': "http://jira.cloudera.com/browse/{{project}}-{{id}}"
   },
   { 
+    'name': 'Hadoop JIRA',
     're': '(hdfs|hive|avro|yarn|hadoop)[- ](\\d+)', 
     'captures': [ "project", "id" ], 
     'link': "https://issues.apache.org/jira/browse/{{project}}-{{id}}"
   },
   { 
     // Doesn't work yet
+    'name': 'JIRA Search (does not work yet)',
     're': 'jira[ ](.+)', 
     'captures': [ "q" ], 
     'link': "http://jira.cloudera.com/browse/QuickSearch.jspa?searchString={{q}}"

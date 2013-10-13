4849edaf40175297c71af496900f690e4f658fef
Philip Zeyliger <philip@cloudera.com>
2013-10-13 15:02:03 -0700

    Cloudera links.

--- data.js
+++ data.js
@@ -1,15 +1,18 @@
-/*
- * schema:
- *    name
- *    re: regular expression, with captures
- *    captures: names of captures
- *    link: mustache template
- */
 var data = [
   { 
-    'name': 'Apache JIRAs',
+    're': '(opsaps|cdh|kitchen|helpdesk)[- ](\\d+)', 
+    'captures': [ "project", "id" ], 
+    'link': "http://jira.cloudera.com/browse/{{project}}-{{id}}"
+  },
+  { 
     're': '(hdfs|hive|avro|yarn|hadoop)[- ](\\d+)', 
     'captures': [ "project", "id" ], 
     'link': "https://issues.apache.org/jira/browse/{{project}}-{{id}}"
+  },
+  { 
+    // Doesn't work yet
+    're': 'jira[ ](.+)', 
+    'captures': [ "q" ], 
+    'link': "http://jira.cloudera.com/browse/QuickSearch.jspa?searchString={{q}}"
   }
 ];
